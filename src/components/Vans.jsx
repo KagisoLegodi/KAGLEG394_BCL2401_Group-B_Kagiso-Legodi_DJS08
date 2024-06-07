35f9424fd7e4ee5a1c607b6ce25e6616e2924b39
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-07 12:47:42 +0200

    Added data fetching logic to the Vans component. It now retrieves van data from the "/api/vans" endpoint and stores it in state using the useEffect hook.

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,6 +1,15 @@
 import React from "react"
 
 export default function Vans() {
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
+    console.log(vans)
+    
     return (
         <h1>Vans page goes here 🚐</h1>
     )

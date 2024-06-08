9a2538fc8334406004fd659a45929951791e2494
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-08 22:54:20 +0200

    Added new routes for Host dashboard, Income, and Reviews pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,24 @@ import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Vans from "./pages/Vans.jsx"
 import VanDetail from "./pages/VanDetail.jsx"
+import Dashboard from "./components//Host/Dashboard.jsx"
+import Income from "./components/Host/Income.jsx"
+import Reviews from "./components/Host/Reviews.jsx"
 import "./server.jsx"
 import Layout from "./components/Layout.jsx"
 
 function App() {
   return (
     <BrowserRouter>
-     <Routes>
+      <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </BrowserRouter>

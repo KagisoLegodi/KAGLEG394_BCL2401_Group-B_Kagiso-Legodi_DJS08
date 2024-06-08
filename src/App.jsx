f9c5f70830ef5dc002be025fc3df1f4f30e306db
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-08 23:16:56 +0200

    chore(Formatting): Apply consistent code style with Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home.jsx"
-import About from "./pages/About.jsx"
-import Vans from "./pages/Vans.jsx"
-import VanDetail from "./pages/VanDetail.jsx"
-import Dashboard from "./components//Host/Dashboard.jsx"
-import Income from "./components/Host/Income.jsx"
-import Reviews from "./components/Host/Reviews.jsx"
-import "./server.jsx"
-import Layout from "./components/Layout.jsx"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Vans from "./pages/Vans.jsx";
+import VanDetail from "./pages/VanDetail.jsx";
+import Dashboard from "./components//Host/Dashboard.jsx";
+import Income from "./components/Host/Income.jsx";
+import Reviews from "./components/Host/Reviews.jsx";
+import "./server.jsx";
+import Layout from "./components/Layout.jsx";
 
 function App() {
   return (
@@ -26,11 +26,9 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-  export default App;
+export default App;

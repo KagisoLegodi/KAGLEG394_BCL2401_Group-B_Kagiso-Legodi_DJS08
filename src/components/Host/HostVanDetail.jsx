e2ac85a716b30db74e227706421ef05169c7fdf8
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-09 03:14:29 +0200

    fix(HostVanDetail): Use Outlet context to share currentVan data

--- src/components/Host/HostVanDetail.jsx
+++ src/components/Host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Link, NavLink, Outlet} from "react-router-dom";
+import { useParams, Link, NavLink, Outlet } from "react-router-dom";
 
 export default function HostVanDetail() {
   const { id } = useParams();
@@ -8,8 +8,8 @@ export default function HostVanDetail() {
   const activeStyles = {
     fontWeight: "bold",
     textDecoration: "underline",
-    color: "#161616"
-}
+    color: "#161616",
+  };
 
   React.useEffect(() => {
     fetch(`/api/host/vans/${id}`)
@@ -20,12 +20,9 @@ export default function HostVanDetail() {
   if (!currentVan) {
     return <h1>Loading...</h1>;
   }
-
   return (
     <section>
-      <Link to=".." 
-      relative="path"
-      className="back-button">
+      <Link to=".." relative="path" className="back-button">
         &larr; <span>Back to all vans</span>
       </Link>
 
@@ -42,30 +39,27 @@ export default function HostVanDetail() {
         </div>
 
         <nav className="host-van-detail-nav">
-                    <NavLink
-                        to="."
-                        end
-                        style={({ isActive }) => isActive ? activeStyles : null}
-                    >
-                        Details
-                    </NavLink>
-                    
-                    <NavLink
-                        to="pricing"
-                        style={({ isActive }) => isActive ? activeStyles : null}
-                    >
-                        Pricing
-                    </NavLink>
-                    
-                    <NavLink
-                        to="photos"
-                        style={({ isActive }) => isActive ? activeStyles : null}
-                    >
-                        Photos
-                    </NavLink>
-                    
-                </nav>
-        <Outlet />
+          <NavLink
+            to="."
+            end
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Details
+          </NavLink>
+          <NavLink
+            to="pricing"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Pricing
+          </NavLink>
+          <NavLink
+            to="photos"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Photos
+          </NavLink>
+        </nav>
+        <Outlet context={{ currentVan }} />
       </div>
     </section>
   );

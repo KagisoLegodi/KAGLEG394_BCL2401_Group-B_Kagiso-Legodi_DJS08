a6e5106f4829a17ca1127f3d30ce4c76868716f4
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-09 02:26:30 +0200

    feat(Routing): Add nested routes for managing individual host vans

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Vans from "./pages/Vans.jsx";
 import VanDetail from "./pages/VanDetail.jsx";
-import Dashboard from "./components/Host/Dashboard"
+import Dashboard from "./components/Host/Dashboard.jsx"
 import Income from "./components/Host/Income.jsx";
 import Reviews from "./components/Host/Reviews.jsx";
 import "./server.jsx";
@@ -12,6 +12,9 @@ import Layout from "./components/Layout.jsx";
 import HostLayout from "./components/HostLayout.jsx";
 import HostVans from "./components/Host/HostVans.jsx";
 import HostVanDetail from "./components/Host/HostVanDetail.jsx";
+import HostVanInfo from "./components/Host/HostVanInfo.jsx";
+import HostVanPricing from "./components/Host/HostVanPricing.jsx";
+import HostVanPhotos from "./components/Host/HostVanPhotos.jsx";
 
 function App() {
   return (
@@ -28,8 +31,12 @@ function App() {
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
-          </Route>
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+        </Route>
         </Route>
       </Routes>
     </BrowserRouter>

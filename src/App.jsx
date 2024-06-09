9f57fa5d979582952720f3c4f13ecf0797021191
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-09 12:31:37 +0200

    Implemented authentication for the host section of the application. Users must now be logged in to access the host dashboard and related pages

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import HostVanInfo from "./pages/Host/HostVanInfo.jsx";
 import HostVanPricing from "./pages/Host/HostVanPricing.jsx";
 import HostVanPhotos from "./pages/Host/HostVanPhotos.jsx";
 import NotFound from "./pages/NotFound"
+import AuthRequired from "./components/AuthRequired"
 
 function App() {
   return (
@@ -33,17 +34,20 @@ function App() {
             element={<Login />}
           />
 
-           <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
+<Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
-        </Route>
+          </Route>
+
         <Route path="*" element={<NotFound />}/>
         </Route>
       </Routes>

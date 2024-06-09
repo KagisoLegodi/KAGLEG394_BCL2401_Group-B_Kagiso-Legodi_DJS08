533eb5f47af8976568de79ae8d0bda0271b50b2d
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-09 03:52:33 +0200

    Ensures that clicking a van tile navigates to the correct detail page: /vans/{vanId}

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -18,18 +18,18 @@ export default function Vans() {
         ? vans.filter(van => van.type === typeFilter)
         : vans
 
-    const vanElements = displayedVans.map(van => (
-        <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
-                <img src={van.imageUrl} />
-                <div className="van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}<span>/day</span></p>
-                </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            </Link>
-        </div>
-    ))
+        const vanElements = displayedVans.map(van => (
+            <div key={van.id} className="van-tile">
+                <Link to={van.id}>
+                    <img src={van.imageUrl} />
+                    <div className="van-info">
+                        <h3>{van.name}</h3>
+                        <p>${van.price}<span>/day</span></p>
+                    </div>
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                </Link>
+            </div>
+        ))
 
     function handleFilterChange(key, value) {
         setSearchParams(prevParams => {

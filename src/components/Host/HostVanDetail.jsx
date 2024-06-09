d84d604b2cfab6f402b41ab04ff0376c1f8823ca
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-09 02:14:07 +0200

    feat(Host): Add components for host van management

--- src/components/Host/HostVanDetail.jsx
+++ src/components/Host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, Outlet} from "react-router-dom";
 
 export default function HostVanDetail() {
   const { id } = useParams();
@@ -34,6 +34,7 @@ export default function HostVanDetail() {
             <h4>${currentVan.price}/day</h4>
           </div>
         </div>
+        <Outlet />
       </div>
     </section>
   );

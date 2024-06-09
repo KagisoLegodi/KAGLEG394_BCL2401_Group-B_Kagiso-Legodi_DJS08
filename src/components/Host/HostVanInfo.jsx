0874b134a05cd13e5cb1a54fedcc3c8c74ee25a1
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-09 03:15:06 +0200

    Implemented HostVanInfo component to render specific van details

--- src/components/Host/HostVanInfo.jsx
+++ src/components/Host/HostVanInfo.jsx
@@ -1,7 +1,13 @@
 //import React from "react"
-
+import { useOutletContext } from "react-router-dom"
 export default function HostVanInfo() {
+    const { currentVan } = useOutletContext()
     return (
-        <h2>Detailed info goes here</h2>
-    )
+        <section>
+        <h4>Name: {currentVan.name}</h4>
+        <h4>Category: {currentVan.type}</h4>
+        <h4>Description: {currentVan.description}</h4>
+        <h4>Visibility: public</h4>
+    </section>
+)
 }
